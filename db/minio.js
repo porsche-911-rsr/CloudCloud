146fa01681d00aca3cca44935a3c320fed4feb40
Egor <runtfint.runtfint@mail.ru>
2024-03-23 04:38:58 +0500

    fix

--- db/minio.js
+++ db/minio.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config()
 export const minioClient = new Minio.Client({
     endPoint: process.env.MINIO_HOST,
-    port: process.env.MINIO_PORT,
+    port: 9000,
     useSSL: false,
     accessKey: process.env.MINIO_ACCESS,
     secretKey: process.env.MINIO_SECRET
