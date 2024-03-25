460b3f48ebafd6fce5f8744cee6f99ac8e23502e
Egor <runtfint.runtfint@mail.ru>
2024-03-26 00:37:49 +0500

    fix

--- ulits/uploadFilesToMinioAndGetLink.js
+++ ulits/uploadFilesToMinioAndGetLink.js
@@ -3,11 +3,13 @@ import {minioClient} from "../db/minio.js";
 
 export const uploadFileAndGetLink = (fileName, filePath, file) => {
     return new Promise((resolve, reject) => {
-        minioClient.fPutObject('logs', fileName, filePath, {'Content-Type': file.mimetype,}, function(err, etag) {
-            if (err)  return reject(err)
-            if (etag) console.log(etag)
-        })
-        const url = minioClient.presignedGetObject('logs', fileName)
-        resolve(url)
+        minioClient.fPutObject('logs', fileName, filePath, {'Content-Type': file.mimetype}, function(err, etag) {
+            if (err) {
+                reject(err);
+            } else {
+                const url = minioClient.presignedGetObject('logs', fileName);
+                resolve(url)
+            }
+        });
     })
 }
